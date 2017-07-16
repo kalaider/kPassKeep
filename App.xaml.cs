65279bd717b48371589ae27442857e7b6052768e
kalaider <a.kalaider@yandex.ru>
2017-07-16 22:24:06 +0300

    Serilog configuration provided
    An initial configuration of logger provided.
    
    Some system and program startup information added to log.

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Serilog;
 
 namespace kPassKeep
 {
@@ -13,5 +14,28 @@ namespace kPassKeep
     /// </summary>
     public partial class App : Application
     {
+
+        static App()
+        {
+            var loglevel = Serilog.Events.LogEventLevel.Error;
+            var args = Environment.GetCommandLineArgs();
+            if (args.Contains("-v") || args.Contains("/v") || args.Contains("--v"))
+            {
+                loglevel = Serilog.Events.LogEventLevel.Verbose;
+            }
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Is(loglevel)
+                .WriteTo.Console()
+                .WriteTo.RollingFile("log/log-{Date}.txt")
+                .CreateLogger();
+
+            Log.Information("Application loaded");
+            Log.Verbose("Current directory: {Dir:l}", Environment.CurrentDirectory);
+            Log.Verbose("Command line: {Start:l}", Environment.CommandLine);
+            Log.Verbose("OS: {OS:l}", Environment.OSVersion);
+            Log.Verbose("CLR: {CLR:l}", Environment.Version);
+            Log.Verbose("System information: {@Env}", Environment.GetEnvironmentVariables());
+        }
+
     }
 }
